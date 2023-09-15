6f367413cf7cee54c2e8c96cf789148eb7d177ec
chafiprc <1538153895@qq.com>
2023-09-15 21:18:42 +0800

    Finish Target 3.1

--- FileIO.h
+++ FileIO.h
@@ -11,6 +11,8 @@
 #ifndef FILE_IO_H
 #define FILE_IO_H
 
+#define MAX_PATH_LENGTH 255
+
 #define GIT_OK 0
 #define GIT_ERROR 1
 
@@ -20,7 +22,28 @@
 #define PATH_IS_DIR 1
 #define PATH_ERROR 2
 
+#define PATH_IS_NOT_EXIST 0
+#define PATH_IS_EXIST 1
+
 #endif
+
+// 拼接字符串，额外申请可见，返回一个新的字符串
+char *StrConcat(char *s1, char *s2)
+{
+    char *newStr = (char *)(malloc(sizeof(char) * MAX_PATH_LENGTH));
+    strcpy(newStr, s1);
+    strcat(newStr, s2);
+    return newStr;
+}
+
+// 判断路径是否存在
+short PathExist(char *path, short *state)
+{
+    if (!access(path, F_OK))
+        return PATH_IS_EXIST;
+    else
+        return PATH_IS_NOT_EXIST;
+}
 
 // 判断路径是文件，还是文件夹
 void PathProperty(char *path, short *state)
@@ -50,7 +73,20 @@ void PathProperty(char *path, short *state)
 void CreateDir(char *path)
 {
     char *command = "mkdir ";
-    system(strcat(command, path));
+    system(StrConcat(command, path));
+}
+
+void CreateGitFile(char *path)
+{
+    char *command = "type nul>";
+    system(StrConcat(command, path));
+}
+
+void WriteDefaultConfig(FILE *file)
+{
+    fprintf(file, "core.repositoryformatversion=0\n");
+    fprintf(file, "core.filemode=false\n");
+    fprintf(file, "core.bare=false\n");
 }
 
 // 读取配置文件
@@ -63,13 +99,40 @@ void ReadConfig(char *path, int *state)
     }
     else // 不存在配置文件，重新创建，并赋予默认值
     {
-        char *command = "type nul>";
-        system(strcat(command, path));
+        CreateGitFile(path);
     }
 }
 
-void ReadDirContent(char *path)
+// 判断文件夹是否为空，空则返回1
+short DirEmpty(char *path)
 {
+    short dirState = 0;
+    PathProperty(path, &dirState);
+    if (dirState != PATH_IS_DIR)
+    {
+        return 0; // 路径文件夹不存在
+    }
+    DIR *dir = opendir(path);
+    struct dirent *entry;
+    if ((entry = readdir(dir)) != 0)
+    {
+        return 0; // 路径文件夹非空
+    }
+    else
+    {
+        return 1; // 路径文件夹为空
+    }
+}
+
+void ReadDirContent(char *path, char *state)
+{
+    short dirState = 0;
+    PathProperty(path, &dirState);
+    if (dirState != PATH_IS_DIR)
+    {
+        *state = GIT_ERROR;
+        return;
+    }
     char *dirContent;
     DIR *dir = opendir(path);
     struct dirent *entry;
--- RuGit.c
+++ RuGit.c
@@ -1,7 +1,5 @@
 #include "FileIO.h"
 
-#define MAX_PATH_LENGTH 255
-
 short gitState = 0;
 
 struct GitRepository
@@ -11,12 +9,23 @@ struct GitRepository
     char *conf;
 };
 
+char *empty = "";
+
 typedef struct GitRepository GitRep;
+
+void IsGitError()
+{
+    if (gitState == GIT_ERROR)
+    {
+        printf("RUNNING ERROR! Program exit.");
+        exit(0);
+    }
+}
 
 void init(GitRep *newRep, char *path, short force)
 {
     newRep->worktree = path;
-    newRep->gitdir = strcat(path, ".git");
+    newRep->gitdir = StrConcat(path, "\\.git");
 
     short gitdirState;
     PathProperty((*newRep).gitdir, &gitdirState);
@@ -33,20 +42,19 @@ void PrintErrorLog(char *w)
 }
 
 // 返回库文件下的路径
-// r是库类型，path是路径数组，parameterNum是参数个数，returnPath是返回的路径
-void RepoPath(GitRep r, char *path[], int paramaterNum, char *returnPath)
+// r是库类型，path是路径，returnPath是返回的路径
+void RepoPath(GitRep r, char *path, char *returnPath)
 {
     strcpy(returnPath, r.gitdir);
-    for (int i = 0; i < paramaterNum; i++)
-        returnPath = strcat(returnPath, path[i]);
+    strcat(returnPath, path);
 }
 
 // 返回库文件下的路径（文件夹），可以决定是否创建
-// r是库类型，path是路径数组，parameterNum是参数个数，returnPath是返回的路径， mkdir是1时若路径不存在则创建，否则不创建；state是状态码
-void RepoDir(GitRep r, char *path[], int paramaterNum, char *returnPath, short mkdir, short *state)
+// r是库类型，path是路径，returnPath是返回的路径， mkdir是1时若路径不存在则创建，否则不创建；state是状态码
+void RepoDir(GitRep r, char *path, char *returnPath, short mkdir, short *state)
 {
     *state = GIT_OK;
-    RepoPath(r, path, paramaterNum, returnPath);
+    RepoPath(r, path, returnPath);
     short returnPathState = 0;
 
     PathProperty(returnPath, &returnPathState);
@@ -62,36 +70,88 @@ void RepoDir(GitRep r, char *path[], int paramaterNum, char *returnPath, short m
     }
     else
     {
-        *returnPath = "";
+        returnPath = empty;
     }
 }
 
 // 返回库文件下的路径（文件），可以决定是否创建
-// r是库类型，path是路径数组，parameterNum是参数个数，returnPath是返回的路径， mkdir是1时若路径不存在则创建，否则不创建；state是状态码
-void RepoFile(GitRep r, char *path[], int paramaterNum, char *returnPath, short mkdir, short *state)
+// path是路径，filename是文件名， mkdir是1时若路径不存在则创建，否则不创建；state是状态码
+char *RepoFile(char *path, char *filename, short mkdir, short *state)
 {
-    RepoDir(r, path, paramaterNum - 1, returnPath, mkdir, state); // 是文件，因此最后一个参数是文件名
-    if (state == GIT_OK)
+    short dirState = 0;
+    PathProperty(path, &dirState);
+    if (dirState == PATH_IS_DIR)
     {
-        returnPath = strcat(returnPath, path[paramaterNum - 1]);
-        return;
+        return StrConcat(path, filename);
+    }
+    else if (mkdir)
+    {
+        CreateDir(path);
+        return StrConcat(path, filename);
+    }
+    else
+    {
+        gitState = GIT_ERROR;
+        return NULL;
     }
 }
 
 // 创建本地库
-void RepoCreate(char *path)
+GitRep *RepoCreate(char *path)
 {
-    GitRep newRep;
-    init(&newRep, path, 1);
+    GitRep *newRep = (GitRep *)malloc(sizeof(GitRep));
+    init(newRep, path, 1);
 
     short repState = 0;
-    PathProperty(newRep.worktree, &repState);
-    if (!(repState == PATH_IS_DIR))
+    PathProperty(newRep->worktree, &repState);
+    if (PathExist(path, &repState) == PATH_IS_EXIST)
     {
-        gitState = GIT_ERROR;
-        return;
+        if (!(repState == PATH_IS_DIR))
+        {
+            gitState = GIT_ERROR;
+            return NULL;
+        }
+        else if (!DirEmpty(newRep->gitdir))
+        {
+            gitState = GIT_ERROR;
+            return NULL;
+        }
     }
-    // if(empty)
+    else
+    {
+        CreateDir(newRep->worktree);
+    }
+
+    char *tempReturnPath = (char *)malloc(sizeof(char) * MAX_PATH_LENGTH);
+    short mkdirState = 0;
+    RepoDir(*newRep, "\\branches", tempReturnPath, 1, &mkdirState);
+    RepoDir(*newRep, "\\objects", tempReturnPath, 1, &mkdirState);
+    RepoDir(*newRep, "\\refs\\tags", tempReturnPath, 1, &mkdirState);
+    RepoDir(*newRep, "\\refs\\heads", tempReturnPath, 1, &mkdirState);
+
+    FILE *file;
+    char *filePath;
+    short fileState = 0;
+
+    // .git/description
+    filePath = RepoFile(newRep->gitdir, "\\description", 1, &fileState);
+    file = fopen(filePath, "w");
+    fprintf(file, "Unnamed repository; edit this file 'description' to name the repository.\n");
+    fclose(file);
+
+    //.git/head
+    filePath = RepoFile(newRep->gitdir, "\\HEAD", 1, &fileState);
+    file = fopen(filePath, "w");
+    fprintf(file, "ref: refs/heads/master\n");
+    fclose(file);
+
+    //.git/config
+    filePath = RepoFile(newRep->gitdir, "\\config", 1, &fileState);
+    file = fopen(filePath, "w");
+    WriteDefaultConfig(file);
+    fclose(file);
+
+    return &newRep;
 }
 
 int main()
@@ -99,6 +159,8 @@ int main()
 
     int s;
     // PathProperty("D:\\SoftLib\\Code\\Project\\RuGit\\.git\\config", &s);
-    ReadDirContent("D:\\SoftLib");
+    // ReadDirContent("D:\\SoftLib");
+    RepoCreate("C:\\Users\\chafiprc\\Desktop\\newGitRepo");
+    printf("%d", gitState);
     // printf("%d", s);
 }
